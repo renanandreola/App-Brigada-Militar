afcaa48c4042b6f88ce5120181acbb8a9e0b4808
MarlonAngonese <marlonangonese.dev@gmail.com>
2022-09-17 04:55:49 -0300

    Configure database and models
    Hash password with bcrypt; generate ObjectID instead id autoincrements; User insert, authenticate and search

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -1,12 +1,25 @@
-import 'package:sqflite/sqflite.dart' as sqflite;
-import 'package:path/path.dart' as path;
+import 'package:app_brigada_militar/database/copy_api_database.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+/**
+ * Class DB
+ * 
+ * Main class that manages the sqlite database
+ */
 class DB {
-  // Create database unique instance
+  /**
+   * Create a unique instance database
+   */
   DB._();
   static final DB instance = DB._();
-  static sqflite.Database? _database;
+  static Database? _database;
 
+  /**
+   * Get database 
+   * 
+   * @return SqliteDatabaseBase database
+   */
   get database async {
     if (_database != null) return _database;
 
@@ -14,32 +27,13 @@ class DB {
   }
 
   /**
-   * Start a database
+   * Start a database or create one if not exists
    */
   _initDatabase() async {
-    print(await sqflite.getDatabasesPath());
-    return await sqflite.openDatabase(
-      path.join("/storage/emulated/0/Download", 'novorumo.db'),
+    return await openDatabase(
+      join("/storage/emulated/0/Download", 'defaultdb.db'),
       version: 1,
-      onCreate: _onCreateDatabase,
+      onCreate: CopyAPIDatabase().executeAll,
     );
   }
-
-  /**
-   * Create database commands
-   */
-  _onCreateDatabase(db, version) async {
-    await db.execute(_user);
-    await db
-        .insert('users', {'email': 'diogola3@gmail.com', 'password': '12345'});
-  }
-
-  // Define User Table
-  String get _user => '''
-    CREATE TABLE users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email VARCHAR(255),
-      password VARCHAR(255)
-    );
-  ''';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:app_brigada_militar/database/models/User.dart';
 import 'package:flutter/material.dart';
 import 'initialPage.dart';
 
 void main(List<String> args) {
+  debugDatabaseBeforeApp();
+
   runApp(MaterialApp(
     home: InitialPage(),
     debugShowCheckedModeBanner: false,
   ));
+}
+
+void debugDatabaseBeforeApp() async {
+  print(await searchUser(email: "renan@gmail.com"));
 }
