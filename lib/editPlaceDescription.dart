7d66b7394547b732bb367b2869389a75f1674526
Renan <renanandreolla@outlook.com>
2022-10-20 22:09:19 -0300

    New screens to edit properties

---
+++ lib/editPlaceDescription.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+import 'dart:developer';
+
+class EditPlaceDescription extends StatefulWidget {
+  // const EditPlaceDescription({Key? key}) : super(key: key);
+  Map formData;
+  EditPlaceDescription(this.formData);
+
+  @override
+  State<EditPlaceDescription> createState() => _EditPlaceDescriptionState();
+}
+
+class _EditPlaceDescriptionState extends State<EditPlaceDescription> {
+  TextEditingController _quantityDefensive = TextEditingController();
+  TextEditingController _gunPlaceDescription = TextEditingController();
+
+  bool _hasDefensive = false;
+  bool _hasGun = false;
+  bool _hasGunPlace = false;
+  bool _hasVehicle = false;
+  int numberDefensives = 0;
+  int numberGun = 0;
+
+  void _goToAdditionalInfoOrVehicle() {
+    // Retrieve form data
+    Map formData = widget.formData;
+
+    // Set new form data
+    Map pageFormData = {
+      'qty_agricultural_defensives': numberDefensives,
+      'has_gun': _hasGun,
+      'has_gun_local': _hasGunPlace,
+      'gun_local_description': _gunPlaceDescription.text,
+    };
+
+    // Merge form
+    formData.addAll(pageFormData);
+
+    // if (_hasVehicle) {
+    //   Navigator.push(
+    //       context, MaterialPageRoute(builder: (context) => Vehicle(formData)));
+    // } else {
+    //   Navigator.push(context,
+    //       MaterialPageRoute(builder: (context) => AditionalInfo(formData)));
+    // }
+  }
+
+  Widget qtdDefensives() {
+    List<TextFormField> filhos = [];
+    for (int i = 1; i <= numberDefensives; i++) {
+      filhos.add(
+        TextFormField(
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Preencha o número de defensivos agrícolas';
+            }
+            return null;
+          },
+          cursorColor: Colors.black,
+          decoration: InputDecoration(
+            labelText: "Quantidade de defensivos agrícolas",
+            labelStyle: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 1),
+                fontSize: 15,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.w400,
+                fontFamily: "RobotoFlex"),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 177, 177, 177)),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 177, 177, 177)),
+            ),
+          ),
+          keyboardType: TextInputType.number,
+          controller: _quantityDefensive,
+        ),
+      );
+    }
+    return Column(
+      children: filhos,
+    );
+  }
+
+  Widget gunInfos() {
+    List<Column> filhos = [];
+    for (int i = 1; i <= numberGun; i++) {
+      filhos.add(Column(
+        children: [
+          // Possui local adequado para  armazenar a arma de fogo
+          Padding(
+              padding: EdgeInsets.only(left: 5, right: 32, top: 5),
+              child: CheckboxListTile(
+                title: Text(
+                    "Possui local adequado para  armazenar a arma de fogo"),
+                activeColor: Color.fromARGB(255, 27, 75, 27),
+                value: _hasGunPlace,
+                onChanged: (newValue) {
+                  setState(() {
+                    _hasGunPlace = newValue!;
+                  });
+                },
+                controlAffinity:
+                    ListTileControlAffinity.leading, //  <-- leading Checkbox
+              )),
+
+          // Descrição do local onde a arma está armazenada
+          Padding(
+            padding: EdgeInsets.only(left: 30, right: 32, top: 5),
+            child: Text("Descrição do local onde a arma está armazenada",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromARGB(255, 120, 120, 120),
+                    fontFamily: "RobotoFlex")),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30, right: 32, top: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  border: Border.all(
+                      color: Colors.black,
+                      width: 1.0,
+                      style: BorderStyle.solid)),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                minLines: 5,
+                maxLines: 5,
+                controller: _gunPlaceDescription,
+              ),
+            ),
+          ),
+        ],
+      ));
+    }
+    return Column(
+      children: filhos,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // title: new Center(
+        //     child: new Text('NOVO RUMO', textAlign: TextAlign.center)),
+        title: Text("Editar Descrição do Local"),
+        backgroundColor: Color.fromARGB(255, 27, 75, 27),
+        leading: GestureDetector(
+          onTap: () {/* Write listener code here */},
+          child: Icon(
+            Icons.menu, // add custom icons also
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                // Color bar
+                Image.asset('assets/images/rectangle.png'),
+
+                // Title
+                Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                    child: Row(
+                      children: [
+                        Text("Editar Descrição do Local",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "RobotoFlex")),
+                      ],
+                    )),
+
+                // Mantém defensivo(s) agrícola(s)
+                Padding(
+                    padding: EdgeInsets.only(left: 5, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Mantém defensivo(s) agrícola(s)"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasDefensive,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasDefensive = newValue!;
+                          _hasDefensive
+                              ? numberDefensives = 1
+                              : numberDefensives = 0;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Show defensives
+                Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                    child: qtdDefensives()),
+
+                // Mantém arma de fogo na residência
+                Padding(
+                    padding: EdgeInsets.only(left: 5, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Mantém arma de fogo na residência"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasGun,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasGun = newValue!;
+                          _hasGun ? numberGun = 1 : numberGun = 0;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Show gun infos
+                Padding(
+                    padding: EdgeInsets.only(left: 0, right: 32, top: 5),
+                    child: gunInfos()),
+
+                // Possui veículo
+                Padding(
+                    padding: EdgeInsets.only(left: 5, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Possui veículo"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasVehicle,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasVehicle = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+                // Next
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 35),
+                  child: ElevatedButton(
+                    child: Text(
+                      'Próximo',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "RobotoFlex"),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 27, 75, 27),
+                      elevation: 2,
+                      fixedSize: Size(330, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: _goToAdditionalInfoOrVehicle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/editProperties.dart
+++ lib/editProperties.dart
@@ -1,3 +1,5 @@
+import 'package:app_brigada_militar/editMachines.dart';
+import 'package:app_brigada_militar/editPlaceDescription.dart';
 import 'package:flutter/material.dart';
 
 class EditProperties extends StatefulWidget {
@@ -8,10 +10,332 @@ class EditProperties extends StatefulWidget {
 }
 
 class _EditPropertiesState extends State<EditProperties> {
+  TextEditingController _respName = TextEditingController();
+  TextEditingController _respLastName = TextEditingController();
+  TextEditingController _quantityResidents = TextEditingController();
+
+  String _dropDownValue = '';
+  bool _hasSign = false;
+  bool _hasCams = false;
+  bool _hasPhoneSignal = false;
+  bool _hasNetwork = false;
+  bool _hasMachines = false;
+
+  void _goToMachinesOrPlaceDescription() {
+    // Create new form data
+    Map formData = {
+      'firstname': _respName.text,
+      'lastname': _respLastName.text,
+      'qty_people': _quantityResidents.text,
+      'has_geo_board': _hasSign,
+      'has_cams': _hasCams,
+      'has_phone_signal': _hasPhoneSignal,
+      'has_internet': _hasNetwork,
+      'property_type': _dropDownValue,
+    };
+
+    if (_hasMachines) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => EditMachines(formData)));
+    } else {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => EditPlaceDescription(formData)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Editar propriedade"),
+      appBar: AppBar(
+        // title: new Center(
+        //     child: new Text('NOVO RUMO', textAlign: TextAlign.center)),
+        title: Text("Editar propriedade"),
+        backgroundColor: Color.fromARGB(255, 27, 75, 27),
+        leading: GestureDetector(
+          onTap: () {/* Write listener code here */},
+          child: Icon(
+            Icons.menu, // add custom icons also
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                // Color bar
+                Image.asset('assets/images/rectangle.png'),
+
+                // Title
+                Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                    child: Row(
+                      children: [
+                        Text("Editar Propriedade",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "RobotoFlex")),
+                      ],
+                    )),
+
+                // ResponsibleName
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Preencha o nome do responsável';
+                      }
+                      return null;
+                    },
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Nome do Responsável",
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 1),
+                          fontSize: 15,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "RobotoFlex"),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.name,
+                    controller: _respName,
+                  ),
+                ),
+
+                // ResponsibleLastname
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Preencha o sobrenome do responsável';
+                      }
+                      return null;
+                    },
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Sobrenome do Responsável",
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 1),
+                          fontSize: 15,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "RobotoFlex"),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.name,
+                    controller: _respLastName,
+                  ),
+                ),
+
+                // Quantity of peoples
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Preencha o número de residentes';
+                      }
+                      return null;
+                    },
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Quantidade de residentes",
+                      labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 1),
+                          fontSize: 15,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "RobotoFlex"),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 177, 177, 177)),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    controller: _quantityResidents,
+                  ),
+                ),
+
+                // Type of Home
+                Padding(
+                    padding: EdgeInsets.only(left: 32, right: 32, top: 5),
+                    child: DropdownButton(
+                      hint: _dropDownValue == null || _dropDownValue == ""
+                          ? Text('Tipo de propriedade',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 1),
+                                  fontSize: 15,
+                                  fontStyle: FontStyle.normal,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: "RobotoFlex"))
+                          : Text(
+                              _dropDownValue,
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 1),
+                                  fontSize: 15,
+                                  fontStyle: FontStyle.normal,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: "RobotoFlex"),
+                            ),
+                      isExpanded: true,
+                      iconSize: 30.0,
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 1),
+                          fontSize: 15,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "RobotoFlex"),
+                      items: ['Casa de Campo', 'Chácara', 'Sítio', 'Residência']
+                          .map(
+                        (val) {
+                          return DropdownMenuItem<String>(
+                            value: val,
+                            child: Text(val),
+                          );
+                        },
+                      ).toList(),
+                      onChanged: (val) {
+                        setState(
+                          () {
+                            _dropDownValue = val.toString();
+                          },
+                        );
+                      },
+                    )),
+
+                // Sign Home
+                Padding(
+                    padding: EdgeInsets.only(left: 15, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Possui placa de georreferenciamento"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasSign,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasSign = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Has cams
+                Padding(
+                    padding: EdgeInsets.only(left: 15, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Há câmeras na residência"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasCams,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasCams = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Has phone signal
+                Padding(
+                    padding: EdgeInsets.only(left: 15, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Funciona sinal telefônico"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasPhoneSignal,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasPhoneSignal = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Has network access
+                Padding(
+                    padding: EdgeInsets.only(left: 15, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Possui acesso a internet"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasNetwork,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasNetwork = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Has machines
+                Padding(
+                    padding: EdgeInsets.only(left: 15, right: 32, top: 5),
+                    child: CheckboxListTile(
+                      title: Text("Há máquina(s) agrícola(s)"),
+                      activeColor: Color.fromARGB(255, 27, 75, 27),
+                      value: _hasMachines,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _hasMachines = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity
+                          .leading, //  <-- leading Checkbox
+                    )),
+
+                // Login
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, top: 35),
+                  child: ElevatedButton(
+                    child: Text(
+                      'Próximo',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "RobotoFlex"),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 27, 75, 27),
+                      elevation: 2,
+                      fixedSize: Size(330, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: _goToMachinesOrPlaceDescription,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
